84c128efa57aac227f32431f1df6e376f63f4e65
Papol14 <johnpaulescalona2@gmail.com>
2024-11-24 22:07:35 +0800

    Create routes from elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Cards from "./components/Cards";
-import JobListings from "./components/JobListings";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(<Route index element={<h1>Sample</h1>} />)
+  );
+
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <Cards />
-      <JobListings />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
-
-      <script src="js/main.js"></script>
+      <RouterProvider router={router} />
     </div>
   );
 };
